e6f346f0d3410338697d1f80c378fcb977d60088
DDubicki <dubickidymitr@gmail.com>
2021-10-05 20:45:44 +0200

    Made new layout for head in HTML. Made new html to list all products. ProductFilter class in progress

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/", "/smartphone", "/tablet", "/smartwatch", "/laptop", "/desktop_computer"})
+@WebServlet(urlPatterns = {"/smartphone", "/tablet", "/smartwatch", "/laptop", "/desktop_computer"})
 public class ProductController extends HttpServlet {
 
     @Override
